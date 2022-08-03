4d9158491f1596a244b772dacdd00c177bdbda4c
tacobayle <nicolas.bayle@gmail.com>
2022-08-03 11:51:00 +0000

    20220801_update_Granular_RBAC

--- variables.tf
+++ variables.tf
@@ -71,7 +71,7 @@ variable "ansible" {
     aviPbAbsentUrl = "https://github.com/tacobayle/ansibleAviClear"
     aviPbAbsentTag = "v1.04"
     aviConfigureUrl = "https://github.com/tacobayle/ansibleAviConfig"
-    aviConfigureTag = "v1.7"
+    aviConfigureTag = "v1.71"
     version = {
       ansible = "5.7.1"
       ansible-core = "2.12.5"
