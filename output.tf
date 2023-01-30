c47badac99e63d9651b9670e0e112ef63917b05e
tacobayle <nicolas.bayle@gmail.com>
2023-01-30 18:59:38 +0000

    20220130_security_demo

--- output.tf
+++ output.tf
@@ -44,9 +44,9 @@ output "ddos_command_with_cookie" {
   value = "requests=\"100\" ; while true ; do rm results.txt ; echo \"sending $requests requests\" ; for i in $(seq 1 $requests) ; do  curl --cookie \"shop_session-id=15cdd4fe-c97e-42b8-b037-de0b197e490a\" -k https://boutique2.vcenter.avi.com -w \"%%{http_code}\\n\" -o /dev/null -s | tee -a results.txt >/dev/null ; done ; echo \"received $(cat results.txt | grep 200 | wc -l) successful responses\"; echo \"---\"; done\n"
 }
 
-output "ddos_command_without_cookie" {
-  value = "requests=\"40\" ; concurrent=\"40\"; while true ; do echo \"sent    : $requests requests\" ; responses=$(ab -v 3 -n $requests -c $concurrent http://boutique.${var.avi.config.vcenter.domains[0].name}/ 2> /dev/null | grep \"LOG: Response code = 200\" | wc -l); echo \"received: $responses successful responses\" ; echo \"---\" ; sleep 1 ; done\n"
-}
+#output "ddos_command_without_cookie" {
+#  value = "requests=\"40\" ; concurrent=\"40\"; while true ; do echo \"sent    : $requests requests\" ; responses=$(ab -v 3 -n $requests -c $concurrent http://boutique.${var.avi.config.vcenter.domains[0].name}/ 2> /dev/null | grep \"LOG: Response code = 200\" | wc -l); echo \"received: $responses successful responses\" ; echo \"---\" ; sleep 1 ; done\n"
+#}
 
 output "destroy" {
   value = "ssh -o StrictHostKeyChecking=no -i ~/.ssh/${basename(var.jump.private_key_path)} -t ubuntu@${vsphere_virtual_machine.jump.default_ip_address} 'git clone ${var.ansible.aviPbAbsentUrl} --branch ${var.ansible.aviPbAbsentTag} ; cd ${split("/", var.ansible.aviPbAbsentUrl)[4]} ; ansible-playbook local.yml --extra-vars @${var.controller.aviCredsJsonFile}' ; sleep 5 ; terraform destroy -auto-approve -var-file=avi.json\n"
