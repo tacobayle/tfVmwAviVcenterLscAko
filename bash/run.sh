c141b714fa5293a1fb635c766ec8b9729defa811
tacobayle <nicolas.bayle@gmail.com>
2023-06-20 13:31:46 +0000

    20230620_latest_update

--- bash/run.sh
+++ bash/run.sh
@@ -1,1 +1,2 @@
-cd ~/tfVmwAviVcenterLscAko ; $(terraform output -json | jq -r .destroy_avi.value) ; sleep 5 ; terraform destroy -auto-approve -var-file=avi.json ; cd .. ; rm -fr tfVmwAviVcenterLscAko ; git clone https://github.com/tacobayle/tfVmwAviVcenterLscAko ; cd tfVmwAviVcenterLscAko ; terraform init ; terraform apply -auto-approve -var-file=avi.json
+cd ~/tfVmwAviVcenterLscAko ; $(terraform output -json | jq -r .destroy_avi.value) ; sleep 5 ;
+terraform destroy -auto-approve -var-file=avi.json ; cd .. ; rm -fr tfVmwAviVcenterLscAko ; git clone https://github.com/tacobayle/tfVmwAviVcenterLscAko ; cd tfVmwAviVcenterLscAko ; terraform init ; terraform apply -auto-approve -var-file=avi.json
